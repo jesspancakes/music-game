2d8e281e9957f0c22d5a3148d73eb5c795a595b4
jesspancakes <c24453362@mytudublin.ie>
2024-11-25 21:17:25 +0000

    added background and button icon changing

--- musicgame/control.gd
+++ musicgame/control.gd
@@ -13,7 +13,58 @@ func _process(delta):
 	
 
 
-func _on_button_pressed() -> void:
+
+func _on_texture_button_pressed() -> void:
+	$"meow-5".play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_2_pressed() -> void:
+	$"meow-4".play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_3_pressed() -> void:
+	$"meow-3".play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_4_pressed() -> void:
+	$"meow-2".play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_5_pressed() -> void:
+	$"meow-1".play
 	
-	
+	pass # Replace with function body.
+
+
+func _on_texture_button_6_pressed() -> void:
+	$meow0.play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_7_pressed() -> void:
+	$meow1.play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_8_pressed() -> void:
+	$meow2.play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_9_pressed() -> void:
+	$meow3.play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_10_pressed() -> void:
+	$meow4.play()
+	pass # Replace with function body.
+
+
+func _on_texture_button_11_pressed() -> void:
+	$meow5.play()
 	pass # Replace with function body.
