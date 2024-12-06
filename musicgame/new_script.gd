0cf2bc02c94622d8c406a0cb233ecc987040324b
jesspancakes <c24453362@mytudublin.ie>
2024-12-06 14:04:35 +0000

    QUIT BUTTON YAYYYY

--- musicgame/new_script.gd
+++ musicgame/new_script.gd
@@ -7,3 +7,8 @@ extends Node
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://node_2d.tscn")
 	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
