598ca343df00890f0dec238cfa81fba78aa35516
jesspancakes <c24453362@mytudublin.ie>
2024-12-07 14:57:33 +0000

    background music will NOT stop playing!

--- musicgame/playpause.gd
+++ musicgame/playpause.gd
@@ -1,7 +1,6 @@
 extends Button
-
 @onready var kevin = $"../kevin"
-@onready var music = $"."
+@onready var button = $"."
 
 var is_playing : bool = true
 
@@ -9,21 +8,20 @@ var is_playing : bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	kevin.play()
-	music.text = "stop"
-	pass # Replace with function body.
-
+	button.text = "stop"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
 func _on_music_pressed() -> void:
 	if is_playing:
 		kevin.stop()
-		music.text = "play"
+		button.text = "play"
 	else:
 		kevin.play()
-		music.text = "stop"
+		button.text = "stop"
 		
 	is_playing = !is_playing
 	pass # Replace with function body.
